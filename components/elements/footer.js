e6275f422bbd64328b78772d8b4dd3d7646d6272
github-project-files <github.project.files@gmail.com>
2022-01-21 14:10:48 +0530

    footer section class name renamed

--- components/elements/footer.js
+++ components/elements/footer.js
@@ -37,29 +37,29 @@ const Footer = ({ footer }) => {
       </div> */}
 	   <section>
     {/* <div class="container text-center"> */}
-      <div class="comm_cont text-center">
+      <div className="comm_cont text-center">
         
             <p>Niraamaya Wellness Retreats</p><p>Corporate Office: Niraamaya Retreats Private Limited, #54, Richmond Road, Bangalore - 560025.</p>
            
-      <div class="col-xs-12">
-        <div class="col-xs-12">
-          <div class="row">
-            <div class="">
-              <ul class="flex flex-row justify-center flex-wrap gap-4 py-6">
+      <div className="col-xs-12">
+        <div className="col-xs-12">
+          <div className="row">
+            <div className="">
+              <ul className="flex flex-row justify-center flex-wrap gap-4 py-6">
               <li>
-                <div class="footer-detail mail-show-hide">
-                  <span class="fa-stack">
-                      <i class="fa fa-square fa-stack-2x"></i>
-                      <i class="fa fa-envelope fa-stack-1x"></i>
+                <div className="footer-detail mail-show-hide">
+                  <span className="fa-stack">
+                      <i className="fa fa-square fa-stack-2x"></i>
+                      <i className="fa fa-envelope fa-stack-1x"></i>
                   </span>
                     <a href="mailto:reservations@niraamaya.com?Subject=Enquiry" target="_top">reservations@niraamaya.com</a>
                   </div>
                 </li>
                 <li>
-                <div class="footer-detail">
-                  <span class="fa-stack">
-                    <i class="fa fa-square fa-stack-2x"></i>
-                    <i class="fa fa-phone fa-stack-1x"></i>
+                <div className="footer-detail">
+                  <span className="fa-stack">
+                    <i className="fa fa-square fa-stack-2x"></i>
+                    <i className="fa fa-phone fa-stack-1x"></i>
                   </span>
                   <span>+91 80 4510 4510</span>
                 </div>
@@ -68,60 +68,60 @@ const Footer = ({ footer }) => {
              </div>
           </div>
         </div>
-      <div class="col-sm-12 footer-row">
-  <div class="social_block col-sm-12 col-md-5">
-      <div class="social_head">
+      <div className="col-sm-12 footer-row">
+  <div className="social_block col-sm-12 col-md-5">
+      <div className="social_head">
         <p>Get In Touch</p>
-        </div><div class="social_cont">
-          <div class="socialIcon flex flex-row justify-center flex-wrap gap-4">
+        </div><div className="social_cont">
+          <div className="socialIcon flex flex-row justify-center flex-wrap gap-4">
           
-              <a class="social-fb" href="https://www.facebook.com/NiraamayaRetreats" target="_blank">
-              <span class="fa fa-facebook">facebook</span></a>
+              <a className="social-fb" href="https://www.facebook.com/NiraamayaRetreats" target="_blank">
+              <span className="fa fa-facebook">facebook</span></a>
 
-              <a class="social-tw" href="https://twitter.com/NiraamayaForU" target="_blank">
-              <span class="fa fa-twitter">twitter</span></a>
+              <a className="social-tw" href="https://twitter.com/NiraamayaForU" target="_blank">
+              <span className="fa fa-twitter">twitter</span></a>
               
-              <a class="social-lin" href="https://www.linkedin.com/company/niraamayalife" target="_blank">
-              <span class="fa fa-linkedin">Linkedin</span></a>
+              <a className="social-lin" href="https://www.linkedin.com/company/niraamayalife" target="_blank">
+              <span className="fa fa-linkedin">Linkedin</span></a>
               
-              <a class="social-ig" href="https://www.instagram.com/niraamayawellnessretreats/" target="_blank">
-              <span class="fa fa-instagram">instagram</span></a>
+              <a className="social-ig" href="https://www.instagram.com/niraamayawellnessretreats/" target="_blank">
+              <span className="fa fa-instagram">instagram</span></a>
             
-              <a class="social-yt" href="https://www.youtube.com/c/NiraamayaInGroup" target="_blank"><span class="fa fa-youtube">Youtube</span></a>
+              <a className="social-yt" href="https://www.youtube.com/c/NiraamayaInGroup" target="_blank"><span className="fa fa-youtube">Youtube</span></a>
               
-              <a class="social-wa" href="https://web.whatsapp.com/send?phone=+91 6364123461" target="_blank"><span class="fa fa-whatsapp">WhatsApp</span></a>
+              <a className="social-wa" href="https://web.whatsapp.com/send?phone=+91 6364123461" target="_blank"><span className="fa fa-whatsapp">WhatsApp</span></a>
 
-              <a class="social-pi" href="https://in.pinterest.com/niraamayaretreats/" target="_blank"><span class="fa fa-pinterest">Pinterest</span></a>
+              <a className="social-pi" href="https://in.pinterest.com/niraamayaretreats/" target="_blank"><span className="fa fa-pinterest">Pinterest</span></a>
           </div>
         </div>
       </div>
     </div>
   </div>
 
-  <div class="col-xs-12 footer-row footer_navbar_wrapper">
-  <div class="clearfix">
-    <nav class="footer_navbar scroll">
-      <ul class="nav navbar-nav flex flex-row justify-center flex-wrap gap-4 py-6">
-        <li class="second-li"><a class="" href="././">Home</a></li>
-        <li class="second-li"><a class="" href="./about-us/about-us.html">About Us</a></li>
-        <li class="second-li"><a class="" href="./private-residences.html">Private Residences</a></li>
-        <li class="second-li"><a class="" href="./ayurveda-packages.html">Ayurveda Packages</a></li>
-        <li class="second-li"><a class="" href="./special-offers.html">Special Offers</a></li>
-        <li class="second-li"><a class="" href="./sustainability/sustainability.html">Sustainability</a></li>
-        <li class="second-li"><a class="" href="https://newtwb.s3.amazonaws.com/images/niraamayasmart/Niraamaya%20Safety%20Protocols%5E.pdf">COVID-19 Safety Measures</a></li>
-        <li class="second-li"><a class="" href="././gallery.html">Gallery</a></li>
-        <li class="second-li"><a class="" href="./faqs/covid-19-measures.html">FAQs</a></li>
-        <li class="second-li"><a class="" href="./media/media.html">Media</a></li>
-        <li class="second-li"><a class="" href="./awards/awards-for-niraamaya-wellness-retreats.html">Awards</a></li><li class="second-li"><a class="" href="./careers/join-us.html">Careers</a></li><li class="second-li"><a class="" href="./privacy-policy/privacy-policy.html">Privacy Policy</a></li>
-        <li class="second-li"><a class="active" href="./blog.html">Blog</a></li>
-        <li class="second-li"><a class="" href="./contact-us/central-reservations.html">Contact Us</a></li>
+  <div className="col-xs-12 footer-row footer_navbar_wrapper">
+  <div className="clearfix">
+    <nav className="footer_navbar scroll">
+      <ul className="nav navbar-nav flex flex-row justify-center flex-wrap gap-4 py-6">
+        <li className="second-li"><a className="" href="././">Home</a></li>
+        <li className="second-li"><a className="" href="./about-us/about-us.html">About Us</a></li>
+        <li className="second-li"><a className="" href="./private-residences.html">Private Residences</a></li>
+        <li className="second-li"><a className="" href="./ayurveda-packages.html">Ayurveda Packages</a></li>
+        <li className="second-li"><a className="" href="./special-offers.html">Special Offers</a></li>
+        <li className="second-li"><a className="" href="./sustainability/sustainability.html">Sustainability</a></li>
+        <li className="second-li"><a className="" href="https://newtwb.s3.amazonaws.com/images/niraamayasmart/Niraamaya%20Safety%20Protocols%5E.pdf">COVID-19 Safety Measures</a></li>
+        <li className="second-li"><a className="" href="././gallery.html">Gallery</a></li>
+        <li className="second-li"><a className="" href="./faqs/covid-19-measures.html">FAQs</a></li>
+        <li className="second-li"><a className="" href="./media/media.html">Media</a></li>
+        <li className="second-li"><a className="" href="./awards/awards-for-niraamaya-wellness-retreats.html">Awards</a></li><li className="second-li"><a className="" href="./careers/join-us.html">Careers</a></li><li className="second-li"><a className="" href="./privacy-policy/privacy-policy.html">Privacy Policy</a></li>
+        <li className="second-li"><a className="active" href="./blog.html">Blog</a></li>
+        <li className="second-li"><a className="" href="./contact-us/central-reservations.html">Contact Us</a></li>
       </ul>
     </nav>
   </div>
   </div>
-  <div class="comm_cont f_bot">
-    <div class="container">
-      <div class="col-xs-12 text-center">
+  <div className="comm_cont f_bot">
+    <div className="container">
+      <div className="col-xs-12 text-center">
         <a href="./sitemap.html" title="click here for sitemap">Sitemap</a><p><a href="//www.simplotel.com" target="_blank">Simplotel - Hotel Website Design &amp; Booking Engine</a></p>
       </div>
     </div>
